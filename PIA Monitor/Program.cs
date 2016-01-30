e27dbd88b60ff36d0136884486fb10a533cf7431
hype-armor <gamm90@gmail.com>
2016-01-29 19:01:48 -0600

    initial commit

---
+++ PIA Monitor/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Windows.Forms;
+
+namespace PIA_Monitor
+{
+    class Program
+    {
+        private static NotifyIcon notifyIcon = new NotifyIcon();
+
+        static void Main(string[] args)
+        {
+            notifyIcon.Visible = true;
+            notifyIcon.Icon = SystemIcons.Asterisk;
+            notifyIcon.MouseClick += notifyIcon_MouseClick;
+            notifyIcon.Text = "PIA Monitor";
+            notifyIcon.BalloonTipTitle = "PIA Monitor";
+            notifyIcon.BalloonTipText = "PIA Monitor";
+
+            // run once
+            T_Elapsed(null, null);
+
+            System.Timers.Timer t = new System.Timers.Timer();
+            t.Elapsed += T_Elapsed;
+            t.Interval = 4010;
+            t.Enabled = true;
+            t.Start();
+
+            while (true)
+            {
+                Console.ReadLine();
+            }
+        }
+
+        private static void T_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            try
+            {
+                bool isUp = PIAUp();
+                
+                string appPath = @"PATH TO APPLICATION";
+                string appName = appPath.Split(new char[] { '\\' }, StringSplitOptions.RemoveEmptyEntries).Last().Replace(".exe","");
+
+
+                if (isUp)
+                {
+                    notifyIcon.Text = "PIA is connected.";
+
+                    // if PIA is up, check if qBittorrent is running. If not, start it.
+                    Process[] localByName = Process.GetProcessesByName(appName);
+                    if (localByName.Count() == 0)
+                    {
+                        ShowBalloon("PIA Monitor", "PIA is UP, starting " + appName + "!");
+                        Process.Start(appPath);
+                        Console.WriteLine(DateTime.Now + " Started: " + appName);
+                    }
+                    notifyIcon.Icon = SystemIcons.Shield;
+                }
+                else
+                {
+                    notifyIcon.Text = "PIA is NOT connected.";
+                    notifyIcon.Icon = SystemIcons.Error;
+                    Process[] localByName = Process.GetProcessesByName(appName);
+
+                    foreach (var item in localByName)
+                    {
+                        ShowBalloon("PIA Monitor", "PIA is DOWN, killing " + appName);
+                        item.Kill();
+                        Console.WriteLine(DateTime.Now + " Killed: " + item.ProcessName);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                notifyIcon.Icon = SystemIcons.Error;
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void notifyIcon_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private static bool PIAUp()
+        {
+            try
+            {
+                NetworkInterface ni = NetworkInterface.GetAllNetworkInterfaces()[0];
+
+                foreach (NetworkInterface NI in NetworkInterface.GetAllNetworkInterfaces())
+                {
+                    if (NI.Description.Contains("TAP-Windows"))
+                    {
+                        GatewayIPAddressInformationCollection addr = NI.GetIPProperties().GatewayAddresses;
+                        if (addr.Count() > 0)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                ShowBalloon("Network", e);
+            }
+
+            return false;
+        }
+
+        private static void ShowBalloon(string Title, Exception e)
+        {
+            notifyIcon.BalloonTipTitle = Title;
+            notifyIcon.BalloonTipText = e.Message;
+            notifyIcon.ShowBalloonTip(2000);
+        }
+
+        private static void ShowBalloon(string Title, string Message)
+        {
+            notifyIcon.BalloonTipTitle = Title;
+            notifyIcon.BalloonTipText = Message;
+            notifyIcon.ShowBalloonTip(2000);
+        }
+    }
+}
